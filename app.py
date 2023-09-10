45922c0a5aa8320f5d07b1e677125348884d4426
TakaakiMinato <takaakiminato2@gmail.com>
2023-09-10 18:13:20 +0900

    mypage機能実装第一段階

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
-from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
@@ -17,6 +17,7 @@ login_manager.init_app(app)
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
@@ -30,13 +31,19 @@ class User(UserMixin, db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/index', methods=['GET', 'POST'])
-@login_required
-def index():
-    if request.method == 'GET':
-        posts = Post.query.all()
-        return render_template('index.html', posts=posts)
 
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect('/index')
+    else:
+        return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -55,36 +62,39 @@ def signup():
     else:
         return render_template('signup.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        user = User.query.filter_by(username=username).first()
-        if check_password_hash(user.password, password):
-            login_user(user)
-            return redirect('/index')
-    else:
-        return render_template('login.html')
-
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect('/login')
+    return redirect('/')
+
+@app.route('/index', methods=['GET', 'POST'])
+@login_required
+def index():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        username = current_user.username
+        return render_template('index.html', posts=posts, username=username)
+
+@app.route('/mypage', methods=['GET', 'POST'])
+@login_required
+def mypage():
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('mypage.html', posts=posts)
 
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
     if request.method == 'POST':
+        username = current_user.username
         title = request.form.get('title')
         body = request.form.get('body')
 
         with app.app_context():
             db.create_all()
-            post = Post(title=title, body=body)
+            post = Post(username=username, title=title, body=body)
             db.session.add(post)
             db.session.commit()
 
@@ -92,6 +102,7 @@ def create():
 
     else:
         return render_template('create.html')
+
 
 
 @app.route('/<int:id>/update', methods=['GET', 'POST'])
@@ -106,7 +117,7 @@ def update(id):
             post.title = request.form.get('title')
             post.body = request.form.get('body')
             db.session.commit()
-        return redirect('/index')
+        return redirect('/mypage')
 
 @app.route('/<int:id>/delete', methods=['GET'])
 @login_required
@@ -115,5 +126,5 @@ def delete(id):
         post = Post.query.get(id)
         db.session.delete(post)
         db.session.commit()
-    return redirect('/index')
+    return redirect('/mypage')
 
