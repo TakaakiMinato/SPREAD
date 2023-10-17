ff5463239e01939669d59a01938b801368007142
TakaakiMinato <takaakiminato2@gmail.com>
2023-10-17 23:36:03 +0900

    session削除

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_session import Session
+#from flask_session import Session
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import openai
@@ -11,10 +11,10 @@ app = Flask(__name__)
 # ここに必要な設定を追加
 app.config["SECRET_KEY"] = os.urandom(24)
 #開発環境
-#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:M19970616@localhost/spread_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:M19970616@localhost/spread_db"
 #本番環境
 #公開する場合はURI情報は外部ファイルに移管する必要あり
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:M19970616@spread-rds.cltnutjgc2rp.ap-northeast-1.rds.amazonaws.com/spread_db"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://admin:M19970616@spread-rds.cltnutjgc2rp.ap-northeast-1.rds.amazonaws.com/spread_db"
 app.config["SESSION_TYPE"] = "filesystem"
 
 db = SQLAlchemy(app)
@@ -22,7 +22,7 @@ migrate = Migrate(app, db)
 login_manager = LoginManager()
 #login_manager.login_view = "login"
 login_manager.init_app(app)
-Session(app)
+#Session(app)
 
 # 他の拡張機能を初期化する場合、ここで初期化
 openai.api_key = os.getenv("OPENAI_API_KEY")
